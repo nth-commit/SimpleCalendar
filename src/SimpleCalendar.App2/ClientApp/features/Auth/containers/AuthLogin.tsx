6e25821162b610e5d396523158727b33873b64a3
Michael Fry <michaelfry2002@gmail.com>
2018-05-27 20:18:12 +1200

    Fix react types error

--- src/SimpleCalendar.App2/ClientApp/features/Auth/containers/AuthLogin.tsx
+++ src/SimpleCalendar.App2/ClientApp/features/Auth/containers/AuthLogin.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-// import * as queryString from 'query-string';
+import * as queryString from 'query-string';
 
 export default class AuthLogin extends React.Component<RouteComponentProps<{}>, {}> {
 
     public render() {
-        // const query = queryString.parse(location.toString());
-        // if (query.redirect) {
-        //     localStorage.setItem('login:redirect', query.redirect);
-        // }
+        const query = queryString.parse(location.search);
+        if (query.redirect) {
+            localStorage.setItem('login:redirect', query.redirect);
+        }
 
         return (
             <div>
