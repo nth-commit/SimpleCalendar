12f5ab00174b496e7ec1c0372493f62db8c7f59d
Michael Fry <michaelfry2002@gmail.com>
2018-07-14 11:28:27 +1200

    Remove setRegionByPath action creator

--- SimpleCalendar.App.Administration/ClientApp/src/store/Regions/ActionCreators/index.ts
+++ SimpleCalendar.App.Administration/ClientApp/src/store/Regions/ActionCreators/index.ts
@@ -1,11 +1,9 @@
 import { fetchRegion } from './fetchRegion';
 import { fetchBaseRegionParents } from './fetchBaseRegionParents';
 import { setRegion } from './setRegion';
-import { setRegionByPath } from './setRegionByPath';
 
 export const regionActionCreators = {
   fetchRegion,
   fetchBaseRegionParents,
-  setRegion,
-  setRegionByPath,
+  setRegion
 };
